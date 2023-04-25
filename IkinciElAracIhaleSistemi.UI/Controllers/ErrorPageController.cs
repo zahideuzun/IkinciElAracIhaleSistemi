0c9c3ccc0f1377711b154b83337be12a85d914c8
zahide <108192527+zahideuzun@users.noreply.github.com>
2023-04-25 18:16:02 +0300

    v7

---
+++ IkinciElAracIhaleSistemi.UI/Controllers/ErrorPageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IkinciElAracIhaleSistemi.UI.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        // GET: ErrorPage
+        public ActionResult Error404()
+        {
+            return View();
+        }
+        public ActionResult Error401()
+        {
+	        return View();
+        }
+	}
+}
